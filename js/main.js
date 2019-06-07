6426ce446754451d60b6935c4c2adaa75f798ac6
riiand <riiand.develop@gmail.com>
2019-06-07 18:00:43 +0700

    added jquery

--- js/main.js
+++ js/main.js
@@ -1,20 +1,85 @@
 
 $(function(){
 
-    var header = $('#header'),
-        introH = $('#intro').innerHeight(),
-        scrollOffset = 0;
+    var header       = $('#header'),
+        introH       = $('#intro').innerHeight() - 70,
+        scrollOffset = $(window).scrollTop();
 
-        console.log(introH);
+    // Fixed Header
+    cheakScroll(scrollOffset);
 
     $(window).on('scroll', function(){
 
         scrollOffset = $(this).scrollTop();
 
-        console.log(scrollOffset);
+        cheakScroll(scrollOffset);
 
     });
 
-});
+    function cheakScroll (scrollOffset) {
 
-console.log("Hello world!");
+        if (scrollOffset >= introH) {
+            header.addClass('header_fixed');
+        } else {
+            header.removeClass('header_fixed');
+        }
+    }
+
+    // Smooth Scroll
+    $('[data-scroll]').on('click', function(event) {
+        event.preventDefault();
+
+        var $this       = $(this),
+            blockId     = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $('.nav__link').removeClass('active');
+        $this.addClass('active');
+
+        $('html, body').animate({
+            scrollTop: blockOffset - $('#header').height()
+        }, 1000);
+    });
+
+    // Menu nav toggle
+    $('#nav_toggle').on('click', function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass('nav-toggle_active');
+        $('#nav').toggleClass('nav_active');
+    });
+
+        // Hide menu on clike nav__link
+    $('.nav__link').on('click', function(event) {
+        event.preventDefault();
+
+        $('#nav').removeClass('nav_active');
+        $('#nav_toggle').removeClass('nav-toggle_active');
+    });
+
+    // Collapse
+    $('.accordion__header').click(function(event) {
+        event.preventDefault();
+    
+        var $this = $(this);
+        
+        if ($this.next().hasClass('accordion_active')) {
+            $this.next().removeClass('accordion_active');
+            $this.next().slideUp(350);
+        } else {
+            $this.parent().parent().find('.accordion__content').removeClass('accordion_active');
+            $this.parent().parent().find('.accordion__content').slideUp(350);
+            $this.next().toggleClass('accordion_active');
+            $this.next().slideToggle(350);
+        }
+    });
+
+    // Reviews slider  
+    $('.slider__wrapper').slick({
+        fade: true, 
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+    
+});
